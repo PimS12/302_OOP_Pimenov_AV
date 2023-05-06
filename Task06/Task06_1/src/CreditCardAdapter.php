36f3c8f6d21284df20a98adf675a539edcbf26aa
PimS12 <apimenoffs@gmail.com>
2023-05-06 23:45:57 +0300

    Task06

--- Task06/Task06_1/src/CreditCardAdapter.php
+++ Task06/Task06_1/src/CreditCardAdapter.php
@@ -4,4 +4,20 @@ namespace App;
 
 class CreditCardAdapter implements PaymentAdapterInterface
 {
+    private $creditCard;
+
+    public function __construct(CreditCard $creditCard)
+    {
+        $this->creditCard = $creditCard;
+    }
+
+    public function collectMoney($amount): bool
+    {
+        return $this->creditCard->authorizeTransaction($amount);
+    }
+
+    public function __get($property)
+    {
+        return $this->$property;
+    }
 }
--- Task06/Task06_1/src/PayPalAdapter.php
+++ Task06/Task06_1/src/PayPalAdapter.php
@@ -4,4 +4,20 @@ namespace App;
 
 class PayPalAdapter implements PaymentAdapterInterface
 {
+    private $paypal;
+
+    public function __construct(PayPal $paypal)
+    {
+        $this->paypal = $paypal;
+    }
+
+    public function collectMoney($amount): bool
+    {
+        return $this->paypal->transfer($this->paypal->email, $amount);
+    }
+
+    public function __get($property)
+    {
+        return $this->$property;
+    }
 }
